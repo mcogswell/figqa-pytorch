a0a26477a598565e33a8540822ab3a7d60534faa
Michael Cogswell <michael.a.cogswell@gmail.com>
2018-02-05 12:27:56 -0500

    read results glob

--- scripts/read_results.py
+++ scripts/read_results.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python
 import pickle as pkl
+import glob
 from pprint import pprint
 
-# print out accuracies from some result files
-for dset in ['train1', 'validation1', 'validation2']:
-    for net in ['cnn-lstm', 'rn', 'lstm']:
-        fname = 'data/results/result_{}_{}.pkl'.format(dset, net)
+def main():
+    '''
+    Print out some results.
+    (Put quotes around the first argument if it has * characters.)
+
+    Usage:
+        read_results.py <result_file_glob>
+    '''
+    import docopt, textwrap
+    args = docopt.docopt(textwrap.dedent(main.__doc__))
+
+    fnames = glob.glob(args['<result_file_glob>'])
+    print(fnames)
+    for fname in fnames:
         with open(fname, 'rb') as f:
-            dat = pkl.load(f)
-            print(fname)
-            print(dat['args'].checkpoint_dir)
-            pprint(dat['acc'])
+            try:
+                dat = pkl.load(f)
+                print(fname)
+                print(dat['args'].checkpoint_dir)
+                pprint(dat['acc'])
+            except pkl.PickleError:
+                print('Could not read {}. Is it a pickle file?'.format(fname))
+
+if __name__ == '__main__':
+    main()
